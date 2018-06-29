ff18093d9875b040348488cffa72a3a7741f6970
Eve Shi <qianyiwork@gmail.com>
2018-06-29 17:23:04 +0800

    update

--- src/contanier/App.js
+++ src/contanier/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 class App extends Component {
   state = {
-
+    showP: true,
   };
 
   render() {
     return (
       <div>
+        {this.state.showP ?
+          <p>test/test</p>
+          : null }
+        <p>there is music</p>
       </div>
     );
   }
