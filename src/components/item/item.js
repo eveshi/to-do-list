829285f6bf9c60ddb5c55aa7b1d9eaab97c72b09
Eve Shi <qianyiwork@gmail.com>
2018-07-03 21:37:10 +0800

    update

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 
 class Item extends Component {
-  state = {
-    showP: false,
-  };
+  // state = {
+  //   showP: false,
+  // };
+
+  constructor() {
+    super();
+    this.state = {
+      showP: false,
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler() {
+    this.setState({
+      showP: !this.state.showP,
+    });
+  }
 
   render() {
     return (
       <div>
         {this.state.showP ?
-          <p>test/test</p>
+          <p>123</p>
           : null }
         <p>there is music</p>
+        <button onClick={this.clickHandler}>change</button>
       </div>
     );
   }
--- src/components/item/item.test.js
+++ src/components/item/item.test.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow, configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Item from './item';
 
-it('renders correctly', () => {
-  const tree = renderer.create(<Item />).toJSON();
-  expect(tree).toMatchSnapshot();
+configure({ adapter: new Adapter() });
+
+describe('<Item />', () => {
+  // beforeAll(() => {
+  //   const wrapper = shallow(<Item />);
+  // });
+
+  it('click button to show text', () => {
+    const wrapper = mount(<Item />);
+    expect(wrapper.find('p')).toHaveLength(1);
+    const instance = wrapper.instance();
+
+    instance.clickHandler();
+    wrapper.update();
+    expect(wrapper.find('p')).toHaveLength(2);
+  });
+});
+
+describe('snapshot', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Item />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
